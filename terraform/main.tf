e6f77b067d054c329190636bbe2f18b560da27a4
Gerrit <ggeeraer@gmail.com>
2024-05-17 19:54:23 +0200

    r2d2

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "mlflow-rg"
+  location = "westeurope"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "mlflowstoracc"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "azurermstoragecontainername"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "bankchurners.csv"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = "../data/raw/bank_churners.csv"
+}
+
+
+
