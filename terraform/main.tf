9da7165b67c0e6f3bf61e641a5d80b3e1e2ebd52
Gerrit <ggeeraer@gmail.com>
2024-05-21 08:15:16 +0200

    azure data lake upload ok

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,19 +27,59 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "example" {
-  name                  = "azurermstoragecontainername"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
+resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
+  name               = "data-lake-gen2-filesystem"
+  storage_account_id = azurerm_storage_account.example.id
+
+  properties = {
+    hello = "aGVsbG8="
+  }
 }
 
-resource "azurerm_storage_blob" "example" {
-  name                   = "bankchurners.csv"
-  storage_account_name   = azurerm_storage_account.example.name
-  storage_container_name = azurerm_storage_container.example.name
-  type                   = "Block"
-  source                 = "../data/raw/bank_churners.csv"
+resource "null_resource" "upload_file" {
+  provisioner "local-exec" {
+    command = <<EOT
+      az storage fs file upload --account-name ${azurerm_storage_account.example.name} \
+        --file-system ${azurerm_storage_data_lake_gen2_filesystem.example.name} \
+        --path bank_churners.csv \
+        --source ../data/raw/bank_churners.csv
+    EOT
+  }
+
+  depends_on = [
+    azurerm_storage_data_lake_gen2_filesystem.example
+  ]
 }
+
+
+
+# resource "azurerm_storage_container" "example" {
+#   name                  = "datafolder"
+#   storage_account_name  = azurerm_storage_account.example.name
+#   container_access_type = "private"
+# }
+#
+# resource "azurerm_storage_blob" "example" {
+#   name                   = "bankchurners.csv"
+#   storage_account_name   = azurerm_storage_account.example.name
+#   storage_container_name = azurerm_storage_container.example.name
+#   type                   = "Block"
+#   source                 = "../data/raw/bank_churners.csv"
+# }
+
+# resource "azurerm_storage_container" "example" {
+#   name                  = "azurermstoragecontainername"
+#   storage_account_name  = azurerm_storage_account.example.name
+#   container_access_type = "private"
+# }
+#
+# resource "azurerm_storage_blob" "example" {
+#   name                   = "bankchurners.csv"
+#   storage_account_name   = azurerm_storage_account.example.name
+#   storage_container_name = azurerm_storage_container.example.name
+#   type                   = "Block"
+#   source                 = "../data/raw/bank_churners.csv"
+# }
 
 
 
